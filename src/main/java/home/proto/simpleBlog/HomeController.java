5c05fbebaf123cc737b9f77736c2b86a149046f6
rhinod8368 <rhinod8368@naver>
2022-07-04 23:40:26 +0900

    [2022-07-04] web.xml 공부 스프링 컨테이너 생성과정

--- src/main/java/home/proto/simpleBlog/HomeController.java
+++ src/main/java/home/proto/simpleBlog/HomeController.java
@@ -1,11 +1,25 @@
 package home.proto.simpleBlog;
 
+import java.io.IOException;
+import java.lang.annotation.Annotation;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.MessageSourceResolvable;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.core.ResolvableType;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
